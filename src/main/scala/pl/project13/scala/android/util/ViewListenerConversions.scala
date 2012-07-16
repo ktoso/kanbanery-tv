eae695a107e610ff01031296bb80d519b2e942ee
Konrad Malawski <konrad.malawski@project13.pl>
2012-07-16 21:56:21 +0200

    onLongClick listeners

--- src/main/scala/pl/project13/scala/android/util/ViewListenerConversions.scala
+++ src/main/scala/pl/project13/scala/android/util/ViewListenerConversions.scala
@@ -1,14 +1,14 @@
 package pl.project13.scala.android.util
 
 import android.view.View
-import android.view.View.OnClickListener
+import android.view.View.{OnLongClickListener, OnClickListener}
 import android.widget.{TextView, Button}
 import android.content.DialogInterface
 
 trait ViewListenerConversions extends ViewConversions {
 
   // views
-  implicit def buttonHasOnClick(btn: Button) = new HasOnClick(btn)
+  implicit def hasOnClick(v: View) = new HasOnClick(v)
 
   // listeners
 
@@ -16,11 +16,16 @@ trait ViewListenerConversions extends ViewConversions {
     def onClick(p1: View) { block }
   }
 
+  implicit def fun1_onLongClickListener(block: => Boolean) = new OnLongClickListener {
+    def onLongClick(p1: View) = { block }
+  }
+
   implicit def fun1_Dialog_onClickListener[T](block: () => T) = new DialogInterface.OnClickListener {
     def onClick(p1: DialogInterface, p2: Int) { block() }
   }
 
-  class HasOnClick(btn: Button) {
+  class HasOnClick(btn: View) {
     def onClick(block: => Unit) { btn setOnClickListener block }
+    def onLongClick(block: => Boolean) { btn setOnLongClickListener block}
   }
 }
